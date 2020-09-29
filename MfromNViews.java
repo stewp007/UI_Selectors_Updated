21653fbf04d8d50f7adf0c6ffc1a489c23ad05bc
stewartpowell <stewartpowell@localhost>
2020-09-28 21:03:47 -0700

    An enum of the valid types of presenters for a M-from-N control

---
+++ MfromNViews.java
@@ -0,0 +1,9 @@
+
+public enum MfromNViews {
+    /** Fully-displayed list of labeled checkboxes */
+    FULL,
+    /** Scrollable list of labeled checkboxes */
+    SCROLLING,
+    /** Array of labeled checkboxes */
+    CHECK;
+}
