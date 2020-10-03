cbb88dc95938528ecd3384beecff06a615d9a1a6
stewartpowell <stewartpowell@localhost>
2020-10-02 23:33:18 -0700

    Semantic Control for Many from N choices

--- MfromN.java
+++ MfromN.java
@@ -1,6 +1,3 @@
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * 
  */
@@ -9,40 +6,16 @@ import java.util.List;
  * @author stewartpowell
  *
  */
-public class MfromN {
-
-    /** The set of all possible values */
-    private List<Object> allValues;
-    /** The set of chosen values */
-    private List<Object> currValues;
-    /** The array of Instantiated Views for this Model */
-    private List<UiView> views;
+public class MfromN extends SemanticControl {
 
     /**
      * Constructor for MfromN Model
-     */
-    public MfromN() {
-        this.allValues = new LinkedList<>();
-        this.currValues = new LinkedList<>();
-        this.views = new LinkedList<>();
-    }
-
-    /**
-     * Adds a value to the current list of values
      * 
-     * @param value a value to add to the current Values
+     * @param layout the layout of the Shell: true for horizontal and false for
+     *               vertical
      */
-    public void addValue(Object value) {
-        currValues.add(value);
-    }
-
-    /**
-     * Adds a list of values to the current list of values
-     * 
-     * @param value a value to add to the current Values
-     */
-    public void addValue(List<Object> value) {
-        currValues.addAll(value);
+    public MfromN(boolean layout) {
+        super(layout);
     }
 
     /**
@@ -50,10 +23,11 @@ public class MfromN {
      * 
      * @param view a value to add to the current Values
      */
-    public void addView(MfromNViews view) {
+    public void addView(Views view) {
         switch (view) {
             case FULL:
-                // views.add(new UiView());
+                getViews().add(new FullListView(0, false, this));
+                break;
             case SCROLLING:
                 // views.add(new UiView());
             case CHECK:
@@ -63,57 +37,4 @@ public class MfromN {
         }
     }
 
-    /**
-     * Gets a list of the possible values
-     * 
-     * @return the list of possible values
-     */
-    public List<Object> getAllValues() {
-        return allValues;
-    }
-
-    /**
-     * Sets the list of Possible Values
-     * 
-     * @param allValues the list of the Possible values
-     */
-    public void setAllValues(List<Object> allValues) {
-        this.allValues = allValues;
-    }
-
-    /**
-     * Gets a list of the Current values
-     * 
-     * @return the list of chosen values
-     */
-    public List<Object> getCurrentValues() {
-        return currValues;
-    }
-
-    /**
-     * Sets the list of Current values
-     * 
-     * @param currValues the list of chosen values
-     */
-    public void setValues(List<Object> currValues) {
-        this.currValues = currValues;
-    }
-
-    /**
-     * Gets a list of all the model's views
-     * 
-     * @return the list of views for this model
-     */
-    public List<UiView> getViews() {
-        return views;
-    }
-
-    /**
-     * Sets the list of the model's views
-     * 
-     * @param views the list of view for this model
-     */
-    public void setViews(List<UiView> views) {
-        this.views = views;
-    }
 }
