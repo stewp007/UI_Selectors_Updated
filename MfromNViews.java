5773e8abbc64e3fbba0380eb82969fd24ccb6c74
stewartpowell <stewartpowell@localhost>
2020-10-02 23:39:48 -0700

    Changed to Views

--- MfromNViews.java
+++
@@ -1,9 +0,0 @@
-
-public enum MfromNViews {
-    /** Fully-displayed list of labeled checkboxes */
-    FULL,
-    /** Scrollable list of labeled checkboxes */
-    SCROLLING,
-    /** Array of labeled checkboxes */
-    CHECK;
-}
