b3bb978aadcf17322a247e0e817beffcf6f25f9e
stewartpowell <stewartpowell@localhost>
2020-10-03 19:07:35 -0700

    Trying to implement a list of Scrolling Checkboxes

---
+++ ScrollListView.java
@@ -0,0 +1,156 @@
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * 
+ */
+
+/**
+ * @author stewartpowell
+ *
+ */
+public class ScrollListView extends UiView {
+    /** The number of check boxes of the presenter */
+    private int numBoxes;
+    /** A scrollable group of check boxes */
+    private final ScrolledComposite scrollGroup;
+
+    /**
+     * Constructor for the Full List View class
+     * 
+     * @param numBoxes     the number of check boxes of the presenter
+     * @param buttonLayout Layout of the Buttons: true for horizontal and false for
+     *                     vertical
+     * @param model        the model associated with the shell and display
+     * @param type         the type of view
+     */
+    public ScrollListView(int numBoxes, boolean buttonLayout, SemanticControl model, Views type) {
+        super(buttonLayout, model, type, SWT.V_SCROLL);
+
+        this.scrollGroup = new ScrolledComposite(model.getShell(), SWT.V_SCROLL);
+        /* Possibility to move to its own function */
+        /*
+         * this.scrollGroup.setLayout(new GridLayout(1, false));
+         * this.scrollGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+         * this.scrollGroup.setContent(getButtonGroup());
+         * this.scrollGroup.setExpandHorizontal(true);
+         * this.scrollGroup.setExpandVertical(true);
+         * this.scrollGroup.setMinSize(scrollGroup.computeSize(SWT.DEFAULT,
+         * SWT.DEFAULT));
+         */
+        this.numBoxes = numBoxes;
+    }
+
+    /**
+     * Helper function to see if a button already exists
+     * 
+     * @param label the label attach to the button
+     * @return the button if it exists, or null otherwise
+     */
+    public Button buttonExists(String label) {
+        Button exists = null;
+        for (Button button : getButtons()) {
+            if (button.getText().equals(label)) {
+                button.setVisible(true);
+                exists = button;
+                break;
+            }
+        }
+        return exists;
+    }
+
+    @Override
+    public void addButton(String label) {
+        Button newButton = buttonExists(label);
+        System.out.println("new button = " + newButton);
+        if (newButton == null) {
+            newButton = new Button(getButtonGroup(), SWT.CHECK);
+            newButton.setText(label);
+            newButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+            newButton.setVisible(true);
+            this.getButtons().add(newButton);
+            System.out.println(getButtons());
+            this.setNumBoxes(getNumBoxes() + 1);
+        }
+
+        for (Control child : getButtonGroup().getChildren()) {
+            if (child instanceof Button && !child.isVisible()) {
+                child.moveBelow(newButton);
+            }
+        }
+        getButtonGroup().update();
+
+    }
+
+    @Override
+    public void addManyButtons(List<Object> labels) {
+        for (int i = 0; i < labels.size(); i++) {
+            Button newButton = buttonExists((String) labels.get(i));
+            if (newButton == null) {
+                newButton = new Button(getButtonGroup(), SWT.CHECK);
+                newButton.setText(labels.get(i).toString());
+                newButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+                newButton.setVisible(true);
+                this.getButtons().add(newButton);
+                this.setNumBoxes(getNumBoxes() + 1);
+                System.out.println("Added new button: " + labels.get(i).toString());
+            }
+            for (Control child : getButtonGroup().getChildren()) {
+                if (child instanceof Button && !child.isVisible()) {
+                    child.moveBelow(newButton);
+                }
+            }
+            getButtonGroup().update();
+
+        }
+    }
+
+    @Override
+    public void removeButton(String label) {
+        for (Button button : getButtons()) {
+            if (button.getText().equals(label) && button != null) {
+                button.setVisible(false);
+
+                for (Control child : getButtonGroup().getChildren()) {
+                    if (child instanceof Button && child.isVisible()) {
+                        child.moveAbove(button);
+                    }
+                }
+                getScrollGroup().update();
+
+            }
+        }
+    }
+
+    /**
+     * Gets the total number of check boxes
+     * 
+     * @return the total number of check boxes
+     */
+    public int getNumBoxes() {
+        return numBoxes;
+    }
+
+    /**
+     * Sets the total number of check boxes
+     * 
+     * @param numBoxes the total number of check boxes
+     */
+    public void setNumBoxes(int numBoxes) {
+        this.numBoxes = numBoxes;
+    }
+
+    /**
+     * Gets the scrollable group of buttons
+     * 
+     * @return the scrollable group of buttons
+     */
+    public ScrolledComposite getScrollGroup() {
+        return scrollGroup;
+    }
+}
