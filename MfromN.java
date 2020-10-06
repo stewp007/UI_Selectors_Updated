1cf64f5c0a96c191b3835bf2fed4d7aec17232d1
stewartpowell <stewartpowell@localhost>
2020-10-06 01:18:41 -0700

    Added Double List view functionality

--- MfromN.java
+++ MfromN.java
@@ -25,15 +25,27 @@ public class MfromN extends SemanticControl {
     public void addView(Views view, boolean layout) {
         switch (view) {
             case FULL:
-                getViews().add(new FullListView(0, layout, this, Views.FULL));
+                getViews().add(new FullListView(this, Views.FULL));
                 break;
             case SCROLL:
                 getViews().add(new ScrollListView(0, layout, this, Views.SCROLL));
                 break;
-            case CHECK:
-                // views.add(new UiView());
+            case DOUBLE:
+                getViews().add(new DoubleListView(this));
+                break;
             default:
                 System.out.println("Error: Incapatable view selected: " + view);
+        }
+    }
+
+    /**
+     * @param view
+     */
+    public void addView(UiView view) {
+        if ((view.getType() == Views.FULL) || (view.getType() == Views.SCROLL) || (view.getType() == Views.DOUBLE)) {
+            getViews().add(view);
+        } else {
+            System.out.println("Error: Non-valid view");
         }
     }
 
