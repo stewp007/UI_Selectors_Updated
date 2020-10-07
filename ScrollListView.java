8f87fa15b51fab4deb6cbe56e4fa049099e5416d
stewartpowell <stewartpowell@localhost>
2020-10-07 00:56:13 -0700

    functioning version of a scroll-able Full list

--- ScrollListView.java
+++ ScrollListView.java
@@ -2,12 +2,16 @@ import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 
 /**
  * 
@@ -18,127 +22,88 @@ import org.eclipse.swt.widgets.Group;
  *
  */
 public class ScrollListView extends UiView {
-    /** The number of check boxes of the presenter */
-    private int numBoxes;
-    /** The containing group for the scrollGroup and buttons */
-    private final Group viewGroup;
     /** A scrollable group of check boxes */
     private final ScrolledComposite scrollGroup;
-    /** The group of check boxes */
+    /** The group of buttons in the scrollable group */
     private final Composite buttonGroup;
+    /** The name of the group of check boxes */
+    private Label groupLabel;
 
     /**
      * Constructor for the Full List View class
      * 
-     * @param numBoxes     the number of check boxes of the presenter
-     * @param buttonLayout Layout of the Buttons: true for horizontal and false for
-     *                     vertical
-     * @param model        the model associated with the shell and display
-     * @param type         the type of view
+     * @param model the model associated with the shell and display
+     * @param type  the type of view
      */
-    public ScrollListView(int numBoxes, boolean buttonLayout, SemanticControl model, Views type) {
+    public ScrollListView(SemanticControl model, Views type) {
         super(model, type);
-        this.viewGroup = new Group(model.getShell(), SWT.NONE);
-        this.scrollGroup = new ScrolledComposite(this.viewGroup, SWT.V_SCROLL);
-
+        model.getShell().setLayout(new FillLayout());
+        this.scrollGroup = new ScrolledComposite(model.getShell(), SWT.V_SCROLL);
+        this.scrollGroup.setLayout(new GridLayout(1, false));
         this.buttonGroup = new Composite(scrollGroup, SWT.NONE);
-        // this.groupLabel = new Label(buttonGroup, SWT.NONE);
-        this.buttonGroup.setLayout(new GridLayout());
-        this.buttonGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-        /* Possibility to move to its own function */
-        /*
-         * this.scrollGroup.setLayout(new GridLayout(1, false));
-         * this.scrollGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-         */
-        this.scrollGroup.setContent(getButtonGroup());
-        setGroupLayout();
+        buttonGroup.setLayout(new GridLayout(1, false));
+        buttonGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        this.groupLabel = new Label(buttonGroup, SWT.NONE);
+        initViews();
         scrollGroup.setExpandHorizontal(true);
         scrollGroup.setExpandVertical(true);
+        scrollGroup.setContent(buttonGroup);
         scrollGroup.setMinSize(buttonGroup.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-        /*
-         * this.scrollGroup.setExpandHorizontal(true);
-         * this.scrollGroup.setExpandVertical(true);
-         * this.scrollGroup.setMinSize(scrollGroup.computeSize(SWT.DEFAULT,
-         * SWT.DEFAULT));
-         */
-        this.numBoxes = numBoxes;
+
     }
 
     /**
-     * Sets pre-filled layout data for the viewGroup and scrollGroup
+     * Initializes the views of the controller
      */
-    private void setGroupLayout() {
-        viewGroup.setLayout(new GridLayout(1, false));
-        GridData scrollData = new GridData(SWT.FILL, SWT.FILL, true, false);
-        scrollData.heightHint = 400;
-        viewGroup.setLayoutData(scrollData);
-        scrollGroup.setLayout(new GridLayout(1, false));
-        scrollGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-        buttonGroup.setLayout(new GridLayout());
-        this.buttonGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-    }
-
-    /**
-     * Helper function to see if a button already exists
-     * 
-     * @param label the label attach to the button
-     * @return the button if it exists, or null otherwise
-     */
-    public Button buttonExists(String label) {
-        Button exists = null;
-        for (Button button : getButtons()) {
-            if (button.getText().equals(label)) {
-                button.setVisible(true);
-                exists = button;
-                break;
+    public void initViews() {
+        for (Object value : getModel().getAllValues()) {
+            if (value != null) {
+                addButton((String) value);
             }
         }
-        return exists;
     }
 
     @Override
     public void addButton(String label) {
         Button newButton = buttonExists(label);
-        System.out.println("new button = " + newButton);
         if (newButton == null) {
-            newButton = new Button(getButtonGroup(), SWT.CHECK);
+            System.out.println("New Visible: " + label);
+            newButton = new Button(this.getButtonGroup(), SWT.CHECK);
             newButton.setText(label);
-            newButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+            newButton.setLayoutData(new GridData(SWT.BEGINNING, 0, false, false));
             newButton.setVisible(true);
+            newButton.addSelectionListener(new SelectionAdapter() {
+
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    Button source = (Button) e.getSource();
+                    if (source.getSelection()) {
+                        getModel().getCurrValue().add(source.getText());
+                    } else {
+                        getModel().getCurrValue().remove(source.getText());
+                    }
+                }
+            });
             this.getButtons().add(newButton);
-            System.out.println(getButtons());
-            this.setNumBoxes(getNumBoxes() + 1);
-        }
-
-        for (Control child : getButtonGroup().getChildren()) {
-            if (child instanceof Button && !child.isVisible()) {
-                child.moveBelow(newButton);
-            }
-        }
-        getButtonGroup().update();
-
-    }
-
-    @Override
-    public void addManyButtons(List<Object> labels) {
-        for (int i = 0; i < labels.size(); i++) {
-            Button newButton = buttonExists((String) labels.get(i));
-            if (newButton == null) {
-                newButton = new Button(getButtonGroup(), SWT.CHECK);
-                newButton.setText(labels.get(i).toString());
-                newButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-                newButton.setVisible(true);
-                this.getButtons().add(newButton);
-                this.setNumBoxes(getNumBoxes() + 1);
-                System.out.println("Added new button: " + labels.get(i).toString());
-            }
+            this.setNumButtons(getNumButtons() + 1);
+        } else {
+            newButton.setVisible(true);
+            System.out.println("Now Visible: " + newButton.getText());
             for (Control child : getButtonGroup().getChildren()) {
                 if (child instanceof Button && !child.isVisible()) {
                     child.moveBelow(newButton);
                 }
             }
-            getButtonGroup().update();
 
+        }
+        getButtonGroup().update();
+        getScrollGroup().update();
+    }
+
+    @Override
+    public void addManyButtons(List<Object> labels) {
+        for (int i = 0; i < labels.size(); i++) {
+            addButton((String) labels.get(i));
         }
     }
 
@@ -160,24 +125,6 @@ public class ScrollListView extends UiView {
     }
 
     /**
-     * Gets the total number of check boxes
-     * 
-     * @return the total number of check boxes
-     */
-    public int getNumBoxes() {
-        return numBoxes;
-    }
-
-    /**
-     * Sets the total number of check boxes
-     * 
-     * @param numBoxes the total number of check boxes
-     */
-    public void setNumBoxes(int numBoxes) {
-        this.numBoxes = numBoxes;
-    }
-
-    /**
      * Gets the scrollable group of buttons
      * 
      * @return the scrollable group of buttons
@@ -187,25 +134,49 @@ public class ScrollListView extends UiView {
     }
 
     /**
-     * Gets the view group
+     * Gets the buttonGroup
      * 
-     * @return the group containing the scrollcomposite and the buttongroup
+     * @return the buttonGroup
      */
-    public Group getViewGroup() {
-        return viewGroup;
+    public Composite getButtonGroup() {
+        return this.buttonGroup;
     }
 
     /**
-     * Sets the name of the View group
+     * Adds a colored background to the group of buttons
      * 
-     * @param name the name of the View group
+     * @param color the color of the background from SWT class
      */
-    public void setViewGroupName(String name) {
-        getViewGroup().setText(name);
+    public void setGroupBackground(int color) {
+        getButtonGroup().setBackground(Display.getCurrent().getSystemColor(color));
     }
 
-    @Override
-    public Composite getButtonGroup() {
-        return super.getButtonGroup();
+    /**
+     * Gets the name of the group of check boxes
+     * 
+     * @return the name of the group of check boxes
+     */
+    public Label getGroupLabel() {
+        return groupLabel;
     }
+
+    /**
+     * Sets the groupLabel
+     * 
+     * @param label the new label
+     */
+    public void setGroupLabel(Label label) {
+        this.groupLabel = label;
+    }
+
+    /**
+     * Sets the title of the group of buttons
+     * 
+     * @param title the title of the group of check boxes
+     */
+    @Override
+    public void setGroupTitle(String title) {
+        this.groupLabel.setText(title);
+    }
+
 }
