a079d41beeb2be8aaf51897eaebf97fa295d8044
SerenaPang4 <50971848+serenapang4@users.noreply.github.com>
2020-11-02 18:11:37 -0800

    add switch button presenter

--- Views.java
+++ Views.java
@@ -1,4 +1,5 @@
 
+
 /**
  * @author stewartpowell This List of All Presenters used in the Semantic
  *         Control Toolkit
@@ -12,16 +13,18 @@ public enum Views {
     CHECK,
     /** Two Full List views: one for selections, one for choices */
     DOUBLE,
-    /** Array of bullets with labels next to each one */
-    RADIOB,
-    /** Array of rectangular labeled buttons */
-    BOXB,
-    /** Button or symbol that pops up a choose-one menu */
-    POPUP,
-    /** Button that shows current value; clicks go to next value */
-    CIRCLEB,
-    /** Text box that is shows and accepts only valid value-names */
-    TEXTFIELD,
-    /** Toggle button On Off view */
-    TOGGLE;
+	/** Array of bullets with labels next to each one */
+	RADIOB,
+	/** Array of rectangular labeled buttons */
+	BOXB,
+	/** Button or symbol that pops up a choose-one menu */
+	POPUP,
+	/** Button that shows current value; clicks go to next value */
+	CIRCLEB,
+	/** Text box that is shows and accepts only valid value-names */
+	TOGGLE,
+	/**Toggle buttons that lights up if selected*/
+	SWITCH,
+	TEXTFIELD;	
+	
 }
